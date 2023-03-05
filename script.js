9acee1edd9d31deee0cd151b526b6ee5fea25a13
yogeshsapare <117711304+yogeshsapare@users.noreply.github.com>
2023-03-06 00:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+function calculateMinCost(){
+	let ropelengthsStr=document.getElementById('rope-lengths').value;
+	let ropelengthsArr=ropelengthsStr.split(",").map(Number);
+	let n=ropelengthsArr.length;
+
+let pq=[];
+	for(let i=0;i<n;i++){
+		pq.push(ropelengthsArr[i]);
+	}
+pq.sort((a,b)=>a-b);
+
+let res=0;
+while(pq.length>1){
+	let first=pq.shift();
+	let second= pq.shift();
+	res+=first+second;
+	pq.push(first+second);
+	pq.sort((a,b)=>a-b);
+}
+	document.getElementById('result').textContent=res;
+}
